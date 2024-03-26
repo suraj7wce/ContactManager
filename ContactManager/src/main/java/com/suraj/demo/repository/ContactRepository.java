6e27ab4367f4144c01aff944b4958f30794525ba
suraj7wce <114141551+suraj7wce@users.noreply.github.com>
2024-03-26 15:07:34 +0530

    Delete ContactManager directory

--- ContactManager/src/main/java/com/suraj/demo/repository/ContactRepository.java
+++
@@ -1,9 +0,0 @@
-package com.suraj.demo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.suraj.demo.model.Contact;
-
-public interface ContactRepository extends JpaRepository<Contact, Long> {
-}
-
